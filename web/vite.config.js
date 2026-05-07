f46eac5e377040ffb6d34c34ced69d212d8c3843
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-07 19:51:28 +0800

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,6 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        silenceDeprecations: ['import']
+      }
+    }
+  },
   optimizeDeps: {
     include: ['@dennislee928/nothingx-react-components']
   }
